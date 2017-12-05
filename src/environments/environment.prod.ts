a9fc36bc1cf6065580e56531884e4ff49bc6dbd3
eggp <eggprojec@gmail.com>
2017-12-05 08:32:18 +0100

    set separated firebase

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,11 +1,11 @@
 export const environment = {
   production: true,
   firebase: {
-    apiKey: 'AIzaSyDhvesEjMGZkuvmOQiEinYZquv0nmVLhXo',
-    authDomain: 'ngpwa-f7127.firebaseapp.com',
-    databaseURL: 'https://ngpwa-f7127.firebaseio.com',
-    projectId: 'ngpwa-f7127',
-    storageBucket: 'ngpwa-f7127.appspot.com',
-    messagingSenderId: '899512844261'
+    apiKey: 'AIzaSyAKprkbWMpoCT300AeS6-niQoTeX5uwLFI',
+    authDomain: 'ngpwa2.firebaseapp.com',
+    databaseURL: 'https://ngpwa2.firebaseio.com',
+    projectId: 'ngpwa2',
+    storageBucket: 'ngpwa2.appspot.com',
+    messagingSenderId: '262469553124'
   }
 };
